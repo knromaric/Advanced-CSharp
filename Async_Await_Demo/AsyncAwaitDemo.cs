ca4943630b03fbf05be539138b83e4ac4cb68f76
knromaric <nzekeng@gmail.com>
2018-06-28 14:57:59 +0300

    Async/await Demo

---
+++ Async_Await_Demo/AsyncAwaitDemo.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using System;
+
+namespace Async_Await_Demo
+{
+    public class AsyncAwaitDemo
+    {
+        public async Task DoStuff()
+        {
+            await Task.Run(() =>
+            {
+                CountToFifty();
+            });
+
+            //this will not execute until CountToFifty has completed
+            Console.WriteLine("Counting to 50 is completed");   
+        } 
+
+        private static void CountToFifty()
+        {
+            for (int counter = 0; counter < 51; counter++)
+            {
+                Console.WriteLine("BG THREAD: "+ counter);
+            }
+
+        }
+    }
+} 
--- Async_Await_Demo/Program.cs
+++ Async_Await_Demo/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Async_Await_Demo
@@ -10,6 +8,57 @@ namespace Async_Await_Demo
     {
         static void Main(string[] args)
         {
+            #region creating a Thread
+            //Thread t = new Thread(WriteX);
+
+            //t.Start();
+            //for (int i = 0; i < 1000; i++)
+            //{
+            //    Console.Write("O");
+
+            //}    
+            #endregion
+
+            #region ThreadPool (background/foreground)Thread
+            //Thread t = new Thread(Count);// FOREGROUND THREAD
+            //t.Start();
+
+            //Task task = Task.Run(() => // background thread
+            //{
+            //    for(int i = 0; i<8; i++)
+            //    {
+            //        Thread.Sleep(500);
+            //        Console.WriteLine(" - BG - ");
+            //    }
+            //});
+            #endregion
+
+            #region ASYNC/AWAIT
+            var demoAsync = new AsyncAwaitDemo();
+            demoAsync.DoStuff();
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("working on the main thread....");
+            }
+            #endregion
+        }
+
+        private static void Count()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("FG");
+            }
+        }
+
+        public static void WriteX()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(".");
+            }           
         }
     }
 } 
